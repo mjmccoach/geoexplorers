207e0c4a62cdb816b0472845dda44a99b8b015d6
svvishjs <andreww92@live.com>
2020-11-02 19:56:25 +0000

    npm installed vue-svg-map and @svg-maps/world as well as creating the SvgMap.vue component

---
+++ client/src/components/SvgMap.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <radio-svg-map v-model="selectedLocation" :map="World" />
+    <h1 v-if="selectedLocation">{{ matchSelectedCountry.name }}</h1>
+  </div>
+</template>
+
+<script>
+import { RadioSvgMap } from "vue-svg-map";
+import World from "@svg-maps/world";
+
+export default {
+  name: "MyMap",
+	components: {
+    RadioSvgMap
+	},
+	data() {
+		return {
+      World,
+      selectedLocation: null,
+      selectedCountry: null
+    };
+  },
+  computed: {
+    matchSelectedCountry: function() {
+      const list = this.World.locations;
+      const id = this.selectedLocation;
+
+      for (let i=0; i<list.length; i++) {
+        const country = list[i]
+        if(id === country.id) {
+          this.selectedCountry = country;
+        };
+      };
+      return this.selectedCountry;
+    }
+  }
+};
+</script>
+
+<style src="vue-svg-map/dist/index.css"></style>
