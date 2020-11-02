f13dc0fef55d7ca676ba1afdf7e4e4dc9d436a83
Douglas MacIntyre <dougmac21@talk21.com>
2020-11-02 13:28:09 +0000

    Country detail formatting

--- client/src/components/CountryDetail.vue
+++ client/src/components/CountryDetail.vue
@@ -5,9 +5,15 @@
       <img id="country-flag" :src="country.flag" :alt="'Flag of ' + country.name" width="250"/>
     </p>
     <p>
-      {{ country.name }} is a country in the {{ country.subregion }} part of {{ country.region }}.
+      {{ country.name }}, also known as {{ country.altSpellings[1] }}, is a country in the {{ country.subregion }} part of {{ country.region }}.
     <p>
       {{ country.name }} has a population of {{ country.population }} people spread over a surface area of {{ country.area }} square miles.
+    </p>
+    <p>
+      Most people in {{ country.name }} speak {{ country.languages[0].name }}, call themselves {{ country.demonym }} and use the {{ country.currencies[0].name }} as money.
+    </p>
+    <p>
+      {{ country.name }} is part of the {{ country.regionalBlocs[0].name }}, also known as {{ country.regionalBlocs[0].otherNames[2] }}
     </p>
     <p>
       The capital of {{ country.name }} is {{ country.capital }}.
