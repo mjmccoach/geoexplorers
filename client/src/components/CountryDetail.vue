ee8f5e78bf6eed9f0db1ea062425ddef5b94cf29
Douglas MacIntyre <dougmac21@talk21.com>
2020-11-02 14:40:36 +0000

    Conditional formatting corrections.

--- client/src/components/CountryDetail.vue
+++ client/src/components/CountryDetail.vue
@@ -1,13 +1,13 @@
 <template>
   <article v-if="country">
     
-    // BASIC INFO
+    <!-- // BASIC INFO -->
     <h1>{{ country.name }}</h1>
     <p>
       <img id="country-flag" :src="country.flag" :alt="'Flag of ' + country.name" width="250"/>
     </p>
 
-    // INFO ABOUT NAME & REGION
+    <!-- // INFO ABOUT NAME & REGION -->
     <p v-if="country.altspellings > 1">
       {{ country.name }}, also known as {{ country.altSpellings[1] }}, is a country in the {{ country.subregion }} part of {{ country.region }}.
     </p>
@@ -16,29 +16,35 @@
       {{ country.name }} is a country in the {{ country.subregion }} part of {{ country.region }}.
     </p>
 
-    // INFO ABOUT POPULATION & AREA
+    <!-- // INFO ABOUT POPULATION & AREA -->
     <p>
       {{ country.name }} has a population of {{ country.population }} people spread over a surface area of {{ country.area }} square miles.
     </p>
 
-    // INFO ABOUT LANGUAGE, NATIONALITY & CURRENCY
-    <p
-    v-if="country.languages >= 1"
-    >
-      Most people in {{ country.name }} speak {{ country.languages[0].name }}, call themselves {{ country.demonym }} and use the {{ country.currencies[0].name }} as money.
+    <!-- // INFO ABOUT LANGUAGE, NATIONALITY & CURRENCY -->
+    <p>
+      <span v-if="country.languages.length > 0">
+      Most people in {{ country.name }} speak {{ country.languages[0].name }}, 
+      </span>
+      <span v-if="country.demonym !==  null">
+      call themselves {{ country.demonym }} 
+      </span>
+      <span v-if="country.currencies.length >0">
+      and use the {{ country.currencies[0].name }} as money.
+      </span>
     </p>
 
-    // INFO ABOUT REGIONAL BLOCS
-    <p v-if="country.regionalBlocs > 1">
-      {{ country.name }} is part of the {{ country.regionalBlocs[0].name }}, also known as {{ country.regionalBlocs[0].otherNames[2] }}
+    <!-- // INFO ABOUT REGIONAL BLOCS -->
+    <p v-if="country.regionalBlocs.length > 0">
+      {{ country.name }} is part of the {{ country.regionalBlocs[0].name }}.
     </p>
 
-    // INFO ABOUT CAPITAL CITY
+    <!-- // INFO ABOUT CAPITAL CITY -->
     <p>
       The capital of {{ country.name }} is {{ country.capital }}.
     </p>
 
-    // INFO ABOUT BORDERING COUNTRIES
+    <!-- // INFO ABOUT BORDERING COUNTRIES -->
     <p v-if="country.borders.length > 1">
       Learn about one of the {{ country.borders.length }} neighbours of {{ country.name }}:
       <bordering-countries-list :borderingCountries="borderingCountries"></bordering-countries-list>
