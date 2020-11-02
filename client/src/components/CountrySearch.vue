a06909892638426bcbb1aae8eda5b98aabc7b4ad
Douglas MacIntyre <dougmac21@talk21.com>
2020-11-02 21:43:42 +0000

    Prep for search by bloc

--- client/src/components/CountrySearch.vue
+++ client/src/components/CountrySearch.vue
@@ -19,21 +19,21 @@
 		:countries="countries" :selectedRegion="selectedRegion">
 		</region-results-list> -->
 
-		<span>Countries By Region:</span>
+		<!-- <span>Countries By Region:</span>
 		<sub-region-search
 		v-for="(subRegion, index) in subRegions" :subRegion="subRegion" :key="index">
 		</sub-region-search>
 		<sub-region-results-list
 		:countries="countries" :selectedSubRegion="selectedSubRegion">
-		</sub-region-results-list>
-<!-- 
+		</sub-region-results-list> -->
+
 		<span>Countries By Political/Economic Bloc:</span>
 		<bloc-search
 		v-for="(bloc, index) in blocs" :bloc="bloc" :key="index">
 		</bloc-search>
 		<bloc-results-list
-		:blocs="blocs" :selectedBloc="selectedBloc">
-		</bloc-results-list> -->
+		:countries="countries" :selectedBloc="selectedBloc">
+		</bloc-results-list>
 
 		<!-- <span>Countries By Language Spoken:</span>
 		<language-search
@@ -59,10 +59,10 @@ import CountryDetail from './CountryDetail';
 // import FirstLetterResultsList from './FirstLetterResultsList';
 // import RegionSearch from './RegionSearch';
 // import RegionResultsList from './RegionResultsList';
-import SubRegionSearch from './SubRegionSearch';
-import SubRegionResultsList from './SubRegionResultsList';
-// import BlocSearch from './BlocSearch';
-// import BlocSearchResultsList from './BlocResultsList';
+// import SubRegionSearch from './SubRegionSearch';
+// import SubRegionResultsList from './SubRegionResultsList';
+import BlocSearch from './BlocSearch';
+import BlocSearchResultsList from './BlocResultsList';
 // import LanguageSearch from './LanguageSearch';
 // import LanguageSearchResultsList from './LanguageResultsList';
  
@@ -77,10 +77,10 @@ export default {
 			selectedFirstLetter: "",
 			// regions: [],
 			// selectedRegion: "",
-			subRegions: [],
-			selectedSubRegion: "",
-			// blocs: [],
-			// selectedBloc: "",
+			// subRegions: [],
+			// selectedSubRegion: "",
+			blocs: [],
+			selectedBloc: "",
 			// languages: [],
 			// selectedLanguage: "",
 		};
@@ -92,10 +92,10 @@ export default {
 		// 'first-letter-results-list': FirstLetterResultsList,
 		// 'region-search': RegionSearch,
 		// 'region-results-list': RegionResultsList,
-		'sub-region-search': SubRegionSearch,
-		'sub-region-results-list': SubRegionResultsList,
-		// 'bloc-search': BlocSearch,
-		// 'bloc-results-list': BlocResultsList,
+		// 'sub-region-search': SubRegionSearch,
+		// 'sub-region-results-list': SubRegionResultsList,
+		'bloc-search': BlocSearch,
+		'bloc-results-list': BlocResultsList,
 		// 'language-search': LanguageSearch,
 		// 'language-results-list': LanguageSearchResultsList,
 	},	
@@ -112,8 +112,8 @@ export default {
 	},
 	mounted() {
 		// this.getAllRegions();
-		this.getAllSubRegions();
-		// this.getAllBlocs();
+		// this.getAllSubRegions();
+		this.getAllBlocs();
 		// this.getAllLanguages();
 
 	// 	eventBus.$on('first-letter-selected', (letter) => {
@@ -122,12 +122,12 @@ export default {
 	// 	eventBus.$on('region-selected', (region) => {
     // 		this.selectedRegion = region;
     // });
-		eventBus.$on('sub-region-selected', (subRegion) => {
-    		this.selectedSubRegion = subRegion;
-    });
-	// 	eventBus.$on('bloc-selected', (bloc) => {
-    // 		this.selectedBloc = bloc;
+	// 	eventBus.$on('sub-region-selected', (subRegion) => {
+    // 		this.selectedSubRegion = subRegion;
     // });
+		eventBus.$on('bloc-selected', (bloc) => {
+    		this.selectedBloc = bloc;
+    });
 	// 	eventBus.$on('language-selected', (language) => {
     // 		this.selectedLanguage = language;
 	// });
@@ -140,17 +140,18 @@ export default {
 		// 	this.regions = regionArray;
 		// // 	console.log(this.regions);
 		// },
-		getAllSubRegions: function () {
-			let subRegionArray = [...new Set(this.countries.map(element => element.subregion))];
-			subRegionArray.sort();
-			this.subRegions = subRegionArray;
-			console.log(this.subRegions);
-		},
-		// getAllBlocs: function () {
-		// 	let distinctArray = [...new Set(this.countries.map(element => element.bloc))];
-		// 	console.log(distinctArray);
-		// 	distinctArray = this.blocs;
+		// getAllSubRegions: function () {
+		// 	let subRegionArray = [...new Set(this.countries.map(element => element.subregion))];
+		// 	subRegionArray.sort();
+		// 	this.subRegions = subRegionArray;
+		// 	console.log(this.subRegions);
 		// },
+		getAllBlocs: function () {
+			let blocArray = [...new Set(this.countries.map(element => element.bloc))];
+			blocArray.sort()
+			this.blocs = blocArray;
+			console.log(this.blocArray);
+		},
 		// getAllLanguages: function () {
 		// 	let distinctArray = [...new Set(this.countries.map(element => element.language))];
 		// 	console.log(distinctArray);
