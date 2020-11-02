5996545f1f4b76e861bfd9eaf31e346c8dfd4b34
AmyBrowneDesigns <amybrowne20@gmail.com>
2020-11-02 17:19:15 +0000

    adds a timer function but this needs to be changed

--- client/src/components/RandomCountry.vue
+++ client/src/components/RandomCountry.vue
@@ -4,6 +4,7 @@
       <!-- it's flag is <img src=""https://restcountries.eu/data/afg.svg"" alt="flag"></-->
       </p>
   <button v-on:click="picker()">Random Country!</button>
+  <p v-if="randomCountryOnTimer">The country of the day is {{randomCountryOnTimer.name}}</p>
 </div>
 </template>
 
@@ -15,15 +16,29 @@ export default {
     data() {
         return {
             randomCountry: null,
+            randomCountryOnTimer: null,
         }
+    },
+
+    mounted() {
+        this.timerForCountry()
     },
 
     methods: {
     picker: function(){
       var chosenNumber = Math.floor(Math.random() * this.countryInfo.length);
       this.randomCountry = this.countryInfo[chosenNumber];
+    },
+
+    timerForCountry: function() {
+        setInterval(() => {
+            const chosenNumber = Math.floor(Math.random() * this.countryInfo.length)
+            this.randomCountryOnTimer = this.countryInfo[chosenNumber];
+        }, 3000)
     }
-}
+
+    // ,
+    }
 };
 
 </script>
