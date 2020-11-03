ce98f7ea7e4f9fc4c29bb61bce5512fa05ec02be
Douglas MacIntyre <dougmac21@talk21.com>
2020-11-03 20:09:56 +0000

    html tags on advanced search

--- client/src/components/CountrySearch.vue
+++ client/src/components/CountrySearch.vue
@@ -9,28 +9,29 @@ v-if="dataReady"
 			<first-letter-search v-for="(letter, index) in alphabet" :letter="letter" :key="index"></first-letter-search>
 			<first-letter-results-list :countries="countries" :selectedFirstLetter="selectedFirstLetter"></first-letter-results-list>
 		</ul>
+		<button v-on:click="displayAdvancedSearch">Advanced Search</button>
 		<div class="advanced-search">
 			<ul>
 				<!-- <li>Countries By Continent:</li>
-				<region-search v-for="(region, index) in regions" :region="region" :key="index"></region-search>
-				<region-results-list :countries="countries" :selectedRegion="selectedRegion"></region-results-list>
+				<ul><region-search v-for="(region, index) in regions" :region="region" :key="index"></region-search></ul>
+				<ul><region-results-list :countries="countries" :selectedRegion="selectedRegion"></region-results-list></ul>
 		
 				<li>Countries By Region:</li>
-				<sub-region-search v-for="(subRegion, index) in subRegions" :subRegion="subRegion" :key="index"></sub-region-search>
-				<sub-region-results-list :countries="countries" :selectedSubRegion="selectedSubRegion"></sub-region-results-list> -->
+				<ul><sub-region-search v-for="(subRegion, index) in subRegions" :subRegion="subRegion" :key="index"></sub-region-search></ul
+				<ul><sub-region-results-list :countries="countries" :selectedSubRegion="selectedSubRegion"></sub-region-results-list></ul> -->
 
 				<!-- <li>Countries By Political/Economic Bloc:</li>
-				<bloc-search v-for="(bloc, index) in blocs" :bloc="bloc" :key="'bloc' + index"></bloc-search>
-				<bloc-results-list :countries="countries" :selectedBloc="selectedBloc"></bloc-results-list>
+				<ul><bloc-search v-for="(bloc, index) in blocs" :bloc="bloc" :key="'bloc' + index"></bloc-search></ul>
+				<ul><bloc-results-list :countries="countries" :selectedBloc="selectedBloc"></bloc-results-list></ul>
 
 				<li>Countries By Language Spoken:</li>
-				<language-search v-for="(language, index) in languages" :language="language" :key="'language' + index"></language-search>
-				<language-results-list :countries="countries" :selectedLangauge="selectedLanguage"></language-results-list> -->
+				<ul><language-search v-for="(language, index) in languages" :language="language" :key="'language' + index"></language-search></ul>
+				<ul><language-results-list :countries="countries" :selectedLangauge="selectedLanguage"></language-results-list></ul> -->
+
+				<li><input type="text" v-model="search" placeholder="Search Countries.."/></li>
+				<li><country-list :countries="filteredList"></country-list></li>
 			</ul>
 		</div>
-		
-    <input type="text" v-model="search" placeholder="Search Countries.."/>
-		<country-list :countries="filteredList"></country-list>
 		<svg-map :countries="countries"></svg-map>
 		<country-detail :country="country" :borderingCountries="borderingCountries"></country-detail>
   </div>
